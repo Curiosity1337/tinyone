75987f1f9d59df802308553e5d21f11e0c4b39ca
Curiosity1337 <axaxaxalol@gmail.com>
2018-02-19 21:43:34 +0300

    1

---
+++ js/style.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+   $('.nav-menu').click(function () {
+      $('.nav-menu-point').toggle()
+   });
+   $('.icon-link').click(function () {
+       $('.icon-link').hide();
+       $('.download-button').show();
+   });
+
+
+});
